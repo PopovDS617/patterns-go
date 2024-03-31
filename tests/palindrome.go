ed8b8da4c3208515dd01dbaf171f0def4d4066af
Popov Dmitry <popovds.bm@gmail.com>
2024-03-31 11:28:53 +0300

    add test examples

---
+++ tests/palindrome.go
@@ -0,0 +1,19 @@
+package tests
+
+import "unicode"
+
+// IsPalindrome checks whether the input string is a palindrome
+func IsPalindrome(s string) bool {
+	letters := make([]rune, 0, len(s))
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
+			return false
+		}
+	}
+	return true
+}
